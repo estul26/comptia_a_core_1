fc560deecda25e4d4c140de31bc7cab9d4556769
estul26 <estul.ismail@gmail.com>
2026-09-17 23:14:51 -0600

    chore: bump PWA cache for v6.6.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "aplus-uyghur-v6.6.1-reading-position-translation-v5.30";
+const CACHE = "aplus-uyghur-v6.6.2-auto-reading-focus-translation-v5.30";
 const CORE = [
   "/",
   "/index.html",
